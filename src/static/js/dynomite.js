81e4a3ae997e797583c28413c767ee3160add8b2
akbar.ahmed <akbar.ahmed@dynomitedb.com>
2016-02-13 09:01:30 -0800

    Add roadmap feature
    - Add /roadmap/
    - Build roadmap based on content in src/content/roadmap/

--- src/static/js/dynomite.js
+++ src/static/js/dynomite.js
@@ -1,14 +1,20 @@
 $(document).ready(function() {
     $('.collapse').on('show.bs.collapse', function() {
         var id = $(this).attr('id');
-        $('a[href="#' + id + '"]').closest('.panel-heading').addClass('dyno-faq-active');
+        $('a[href="#' + id + '"]').closest('.panel-heading').addClass('dyno-accordion-active');
         $('a[href="#' + id + '"] i').addClass('fa-minus');
         $('a[href="#' + id + '"] i').removeClass('fa-plus');
     });
     $('.collapse').on('hide.bs.collapse', function() {
         var id = $(this).attr('id');
-        $('a[href="#' + id + '"]').closest('.panel-heading').removeClass('dyno-faq-active');
+        $('a[href="#' + id + '"]').closest('.panel-heading').removeClass('dyno-accordion-active');
         $('a[href="#' + id + '"] i').addClass('fa-plus');
         $('a[href="#' + id + '"] i').removeClass('fa-minus');
     });
+});
+
+/* Tabs */
+$('#myTabs a').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show');
 });
